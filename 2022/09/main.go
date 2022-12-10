185fa4ab2ad016eec681f9c5ac43bf3cbc957375
william <william_martineau@hotmail.com>
2022-12-09 20:07:54 -0500

    day 09

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -7,12 +7,14 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
-const FILENAME = "input"
+const FILENAME = "input.dat"
 
 func main() {
-	fmt.Println("Part 1 : ", part1())
+	// fmt.Println("Part 1 : ", part1())
 	fmt.Println("Part 2 : ", part2())
 }
 
@@ -68,17 +70,13 @@ func part1() int {
 }
 
 func part2() int {
-	file, err := os.Open(FILENAME)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	//Read input file
+	input, _ := os.Open(FILENAME)
+	scanner := bufio.NewScanner(input)
+	defer input.Close()
 
-	scanner := bufio.NewScanner(file)
-
-	visited := make(map[Pair]bool, 0)
-	head := Pair{0, 0}
-	tail := make([]Pair, 9)
+	tails := make([]Pair, 10)
+	visited := make(map[Pair]bool)
 
 	for scanner.Scan() {
 		tokens := strings.Split(scanner.Text(), " ")
@@ -95,44 +93,39 @@ func part2() int {
 		}
 		length, _ := strconv.Atoi(tokens[1])
 		for i := 0; i < length; i++ {
-			oldHead := head
-			head = Add(head, direction)
-
-			for i, item := range tail {
-				distance := Distance(head, tail)
-
-				// First iteration
-				switch distance {
-				case 0, 1:
-					break
-				case 2:
-					tail = Add(tail, direction)
-					break
-				case 3:
-					tail = oldHead
-					break
-				}
+			tails[0] = Add(tails[0], direction)
+			for i := 0; i < len(tails)-1; i++ {
+				tails[i+1] = setTail(tails[i+1], tails[i])
 			}
-
-			distance := Distance(head, tail)
-
-			// First iteration
-			switch distance {
-			case 0, 1:
-				break
-			case 2:
-				tail = Add(tail, direction)
-				break
-			case 3:
-				tail = oldHead
-				break
-
-			}
-			visited[tail] = true
+			visited[tails[9]] = true
 		}
 	}
 
 	return len(visited)
+}
+
+func setTail(tail Pair, head Pair) (newTail Pair) {
+	newTail = tail
+	offset := Pair{head.x - tail.x, head.y - tail.y}
+	a := []Pair{{-2, 1}, {-1, 2}, {0, 2}, {1, 2}, {2, 1}, {2, 2}, {-2, 2}}
+	b := []Pair{{1, 2}, {2, 1}, {2, 0}, {2, -1}, {1, -2}, {2, 2}, {2, -2}}
+	c := []Pair{{-2, -2}, {2, -1}, {1, -2}, {0, -2}, {-1, -2}, {-2, -1}, {2, -2}}
+	d := []Pair{{-2, -2}, {-1, -2}, {-2, -1}, {-2, -0}, {-2, 1}, {-1, 2}, {-2, 2}}
+
+	if slices.Contains(a, offset) {
+		newTail.y++
+	}
+	if slices.Contains(b, offset) {
+		newTail.x++
+	}
+	if slices.Contains(c, offset) {
+		newTail.y--
+	}
+	if slices.Contains(d, offset) {
+		newTail.x--
+	}
+
+	return
 }
 
 type Pair struct {
@@ -176,5 +169,20 @@ func printBoard(h, t Pair) {
 		fmt.Println()
 	}
 	fmt.Println()
+}
 
+func printBoard2(a []Pair) {
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 6; j++ {
+			if slices.Contains(a, Pair{j, i}) {
+				fmt.Print("X")
+
+			} else {
+				fmt.Print(".")
+
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
 }
